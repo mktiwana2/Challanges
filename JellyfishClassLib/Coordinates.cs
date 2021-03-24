f20516eadb7fdbbe449bf27fdeb42db0f18c2dc2
Mandeep <abhi@xyz.comjustmkaur@gmail.com>
2021-03-24 22:10:33 +0530

    Change to Coordinates

---
+++ JellyfishClassLib/Coordinates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JellyfishClassLib
+{
+
+
+    public struct Coordinates
+    {
+
+        public int X   // property
+        {
+            get;
+            set;
+        }
+        public int Y   // property
+        {
+            get;
+            set;
+        }
+        public Boolean OutOfRange(int maxX, int maxY)
+        {
+            if (X < 0 || X > maxX || Y < 0 || Y > maxY) return true;
+            else return false;
+        }
+
+    }
+}
--- JellyfishClassLib/TrackJellyfish.cs
+++ JellyfishClassLib/TrackJellyfish.cs
@@ -188,25 +188,4 @@ namespace JellyfishClassLib
     }
 
 
-
-    public struct Coordinates
-    {
-
-        public int X   // property
-        {
-            get;
-            set;
-        }
-        public int Y   // property
-        {
-            get;
-            set;
-        }
-        public Boolean OutOfRange(int maxX, int maxY)
-        {
-            if (X < 0 || X > maxX || Y < 0 || Y > maxY) return true;
-            else return false;
-        }
-
-    }
 }
